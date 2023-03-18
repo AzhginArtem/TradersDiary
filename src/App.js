6360f3fd609f6b76068af01a62f6ef77f5cf10a7
AzhginArtem <azhginartemdeveloper@gmail.com>
2023-03-18 23:02:50 +0300

    fix browserRouter error

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import AppBar from './components/AppBar/AppBar';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <AppBar title="Регистрация" />
       <AppRouter />
       <NavBar />
